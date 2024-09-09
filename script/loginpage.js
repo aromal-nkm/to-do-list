aa0379f5d330cfdffa5d5c128d261297151a27aa
aromal-nkm <aromalts2002@gmail.com>
2024-09-09 14:51:43 +0400

    first commit

---
+++ script/loginpage.js
@@ -0,0 +1,12 @@
+function handleLogin() {
+    var username = document.getElementById('name').value;
+    var password = document.getElementById('pass').value;
+    
+    if (username === 'admin' && password === '12345') {
+        window.location.href = 'list.html'; 
+    } else {
+        document.getElementById('message').innerText = 'Invalid username or password';
+    }
+}
+
+document.getElementById('log').addEventListener('click', handleLogin);
